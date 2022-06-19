97657e412d2b914dac8904a05ded7bbe990d4671
Ruiqi Huang <kiyoakila@gmail.com>
2022-06-19 12:11:43 -0700

    search ui

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,22 @@
-import React from "react";
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
+
+export default Pet;
